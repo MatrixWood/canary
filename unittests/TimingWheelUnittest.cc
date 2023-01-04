6f7f99f4bd8c669614d1bf8646cc9d11f30d2e27
CanftIn <wwc7033@gmail.com>
2023-01-04 07:30:17 +0000

    test: add time wheel test

---
+++ unittests/TimingWheelUnittest.cc
@@ -0,0 +1,36 @@
+#include <memory>
+
+#include "Logger.h"
+#include "TimingWheel.h"
+class MyClass {
+ public:
+  MyClass();
+  MyClass(MyClass &&) = default;
+  MyClass(const MyClass &) = default;
+  MyClass &operator=(MyClass &&) = default;
+  MyClass &operator=(const MyClass &) = default;
+  ~MyClass();
+
+ private:
+};
+
+MyClass::MyClass() {}
+
+MyClass::~MyClass() { LOG_DEBUG << "MyClass destructed!"; }
+
+int main() {
+  LOG_DEBUG << "start";
+  canary::EventLoop loop;
+  std::weak_ptr<MyClass> weakEntry;
+  canary::TimingWheel wheel(&loop, 75, 0.1, 100);
+  {
+    auto entry = std::shared_ptr<MyClass>(new MyClass);
+
+    wheel.insertEntry(75, entry);
+    weakEntry = entry;
+  }
+  loop.runAfter(5.0, [&]() { wheel.insertEntry(10, weakEntry.lock()); });
+  loop.runAfter(0.5, [&]() { wheel.insertEntry(28, weakEntry.lock()); });
+  loop.runEvery(1.0, []() { LOG_DEBUG << "tick"; });
+  loop.loop();
+}
